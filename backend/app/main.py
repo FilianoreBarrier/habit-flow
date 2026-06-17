2fd3b12f4e32cc4f81d3ee00b7aed635dc2cfda8
FilianoreBarrier <maric.zubenko@gmail.com>
2026-06-17 14:38:02 +0500

    change main config

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
-from app.core.config import settings
-from app.core.database import init_db
-from app.routers.v1 import auth, habits, users
+# Импорты внутри пакета app
+from core.config import settings
+from core.database import init_db
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers.v1 import auth, habits, users
 
 app = FastAPI(
     title="HabitFlow",
@@ -10,7 +11,6 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# CORS (чтобы фронтенд мог обращаться к бэкенду)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins,
@@ -19,7 +19,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Подключаем роутеры
 app.include_router(auth.router, prefix="/api/v1", tags=["auth"])
 app.include_router(users.router, prefix="/api/v1", tags=["users"])
 app.include_router(habits.router, prefix="/api/v1", tags=["habits"])
@@ -27,18 +26,10 @@ app.include_router(habits.router, prefix="/api/v1", tags=["habits"])
 
 @app.on_event("startup")
 async def startup_event():
-    init_db()          # Создаёт таблицы в базе данных при запуске
-    print(" HabitFlow запущен. База данных инициализирована.")
+    init_db()
+    print("✅ HabitFlow запущен. База данных инициализирована.")
 
 
 @app.get("/")
 async def root():
-    return {
-        "message": "HabitFlow API работает!",
-        "docs": "/docs"
-    }
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    return {"message": "HabitFlow API работает!", "docs": "/docs"}
