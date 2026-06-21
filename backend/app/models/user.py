26b18288d7c2eca45e46d10d6fc009fde021b97a
FilianoreBarrier <maric.zubenko@gmail.com>
2026-06-21 22:43:13 +0500

    rename user_name -> username

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 class User(Base):
     __tablename__ = "Users"
     user_id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String,nullable=False,index=True)
+    username = Column(String,nullable=False,index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, nullable=True)
