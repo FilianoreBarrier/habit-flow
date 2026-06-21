345a87440698a68550e7eb73d017e6cab7f34aa7
FilianoreBarrier <maric.zubenko@gmail.com>
2026-06-21 23:37:14 +0500

    create Habit & HabitLog schemas

---
+++ backend/app/schemas/habit.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class HabitBase(BaseModel):
+    name: str = Field(max_length=50, description="Habit name")
+    description: Optional[str] = Field(None,max_length=300,description="Habit description")
+    frequency: str = Field(default="daily",description='daily/weekly/monthly')
+class HabitCreate(HabitBase):
+    pass
+class HabitResponse(HabitBase):
+    id: int = Field(description="Unique habit identifier")
+    user_id: int
+    created_at: datetime
+    is_active: bool = True
+    model_config = {"from_attributes": True}
+class HabitUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    is_active: Optional[bool] = None
+    frequency: Optional[str] = None
---
+++ backend/app/schemas/habitlog.py
@@ -0,0 +1,23 @@
+from datetime import date, datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class HabitLogBase(BaseModel):
+    date: date= Field(description="Habit creation date ")
+    completed: bool = Field(default=True,description="Tracking habit completion")
+    note: Optional[str] = Field(None,max_length=100,description='Note')
+class HabitLogCreate(HabitLogBase):
+    pass
+
+class HabitLogResponse(HabitLogBase):
+    id: int
+    habit_id: int
+    user_id: int
+    created_at: datetime
+    model_config = {"from_attributes":True}
+
+class HabitLogUpdate(BaseModel):
+    completed: Optional[bool] = None
+    note: Optional[str] = None
