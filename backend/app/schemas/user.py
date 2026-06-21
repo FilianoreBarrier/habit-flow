d603bd326992f384158a3ebdf0486401bf80212b
FilianoreBarrier <maric.zubenko@gmail.com>
2026-06-21 22:43:52 +0500

    fix UserResponse

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,22 +1,26 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, Field, ValidationError
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserBase(BaseModel):
     username: str = Field(min_length=3, max_length=20, description="Unique username")
     email: EmailStr= Field(description="User email for authorization")
-    full_name: Optional[str] = Field(default=None,max_length=30,description='User full name')
+    full_name: Optional[str] = Field(default=None,max_length=50,description='User full name')
     
 class UserCreate(UserBase):
     password: str = Field(min_length=8,pattern=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$'
 , description='User password for authorization')
-class UserResponse(UserBase):
-    user_id: int = Field(description="Unique user identifier")
+class UserResponse(BaseModel):
+    id: int = Field(description="Unique user identifier")
+    username: str
+    email:EmailStr
+    full_name: Optional[str] = None
     is_active: bool = Field(description='Shows user activity ')
     created_at: datetime
     model_config = {"from_attributes":True}
+    
 
 class UserUpdate(BaseModel):
     full_name: Optional[str] = None
