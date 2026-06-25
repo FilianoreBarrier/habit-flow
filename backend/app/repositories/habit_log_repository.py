05fa5a3ddfacf29c8e7d8607a0d4813eda5e98f1
FilianoreBarrier <maric.zubenko@gmail.com>
2026-06-25 18:03:13 +0500

    rename habitlog_repository -> habit_log_repository. Add class HabitLogRepository

---
+++ backend/app/repositories/habit_log_repository.py
@@ -0,0 +1,49 @@
+from app.models.habit_log import HabitLog
+from app.schemas.habit_log import HabitLogCreate, HabitLogUpdate
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+
+class HabitLogRepository:
+        def __init__(self,db:Session):
+            self.db = db
+
+        def get_by_id(self,log_id:int) -> HabitLog| None:
+              return self.db.get(HabitLog, log_id)
+        
+        def get_by_completed(self, completed:bool) -> HabitLog| None:
+              return self.db.scalar(select(HabitLog).where(HabitLog.completed == completed))
+        
+        def get_by_user_id(self, user_id: int) -> list[HabitLog]:
+            return list(
+                  self.db.scalars(
+                  select(HabitLog)
+                  .where(HabitLog.user_id == user_id)
+                  ).all()
+                  )
+        def get_by_habit_id(self, habit_id: int) -> list[HabitLog]:
+            return list(
+                  self.db.scalars(
+                  select(HabitLog)
+                  .where(HabitLog.habit_id == habit_id)
+                  ).all()
+                  )
+        def create(self, log_data: HabitLogCreate, user_id:int,habit_id:int) -> HabitLog:
+             db_log = HabitLog(**log_data.model_dump(),
+                               user_id = user_id,
+                               habit_id = habit_id
+             )
+             self.db.add(db_log)
+             self.db.commit()
+             self.db.refresh(db_log)
+             return db_log
+        
+        def update(self, log_id: int, log_update: HabitLogUpdate) -> HabitLog | None:
+            habitlog = self.get_by_id(log_id)
+            if habitlog:
+                update_data = log_update.model_dump(exclude_unset=True)
+                for key, value in update_data.items():
+                    setattr(habitlog, key, value)
+                self.db.commit()
+                self.db.refresh(habitlog)
+            return habitlog
