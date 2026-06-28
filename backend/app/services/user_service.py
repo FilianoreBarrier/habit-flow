f9fff1504f7e2f40cba71c9358f6f46d13a87c47
FilianoreBarrier <maric.zubenko@gmail.com>
2026-06-28 17:52:24 +0500

    Added update_user, authenticate_user and get methods.

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from app.core.security import get_password_hash
+from app.core.security import get_password_hash, verify_password
 from app.repositories.habit_repository import HabitRepository
 from app.repositories.user_repository import UserRepository
-from app.schemas.user import UserCreate, UserResponse
+from app.schemas.user import UserCreate, UserResponse, UserUpdate
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
@@ -27,7 +27,48 @@ class UserService:
         hashed_password = get_password_hash(user_data.password)
         user = self.user_repository.create(user_data, hashed_password)
         return UserResponse.model_validate(user)
+    
+    def update_user(self, user_id: int, user_update: UserUpdate) -> UserResponse:
+        user = self.user_repository.get_by_id(user_id)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f'User with id {user_id} not found.'
+            )
+        updated_user = self.user_repository.update(user_id,user_update)
+        return UserResponse.model_validate(updated_user)
+    
+    def change_password():
+        ...
 
+    def authenticate_user(self, email: str, password: str) -> UserResponse:
+        user = self.user_repository.get_by_email(email)
+        if not user:
+            raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password"
+        )
+
+        if not verify_password(password, hashed_password): # type: ignore[attr-defined]
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect email or password"
+            )
+        if not user.is_active:# type: ignore[attr-defined]
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User is inactive"
+            )
+
+        return UserResponse.model_validate(user)
+        
+        
+    def deactivate_user():
+        ...
+    def list_users():
+        ...
+
+    '''get methods'''
     def get_by_id(self, user_id: int) -> UserResponse:
         user = self.user_repository.get_by_id(user_id)
         if not user:
@@ -37,44 +78,26 @@ class UserService:
             )
         return UserResponse.model_validate(user)
     
-    def update_user():
-        ...
-    def get_by_email():
-        ...
-    def authenticate_user():
-        ...
-    def get_by_username():
-        ...
-    def get_current_user():
-        ...
-    def change_password():
-        ...
-    def deactivate_user():
-        ...
-    def list_users():
-        ...
-    
-#     def get_all_products (self) -> ProductListResponse:
-#         products = self.product_repository.get_all()
-#         products_response=[ProductResponse.model_validate(prod) for prod in products]
-#         return ProductListResponse(products = products_response, total=len(products_response))
+    def get_by_email(self, email: str) -> UserResponse:
+        user = self.user_repository.get_by_email(email)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"User with this email not found."
+            )
+        return UserResponse.model_validate(user)
 
-#     def get_product_by_id(self, product_id:int) -> ProductResponse:
-#         product = self.product_repository.get_by_id(product_id)
-#         if not product:
-#             raise HTTPException(
-#                 status_code=status.HTTP_404_NOT_FOUND,
-#                 detail=f'Product with id {product_id} not found'
-#             )
-#         return ProductResponse.model_validate(product)
+    def get_by_username(self, username: str) -> UserResponse:
+        user = self.user_repository.get_by_username(username)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"User with this username not found."
+            )
+        return UserResponse.model_validate(user)
 
-#     def get_product_by_category(self, category_id:int) -> ProductListResponse:
-#         category = self.category_repository.get_by_id(category_id)
-#         if not category:
-#             raise HTTPException(
-#                 status_code=status.HTTP_404_NOT_FOUND,
-#                 detail= f'Category with id {category_id} not found'
-#             )
-#         products = self.product_repository.get_by_category(category_id)
-#         products_response = [ProductResponse.model_validate(prod) for prod in products]
-#         return ProductListResponse(products=products_response, total = len(products_response))
+    def get_all_users(self) -> list[UserResponse]:
+        users = self.user_repository.get_all()
+        return [UserResponse.model_validate(user) for user in users]
+        
+
