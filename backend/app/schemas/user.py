6bc0c31f417b89834389711ecd76c4f1cc1e019d
FilianoreBarrier <maric.zubenko@gmail.com>
2026-06-28 16:55:23 +0500

    Attribute locations have been changed

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserBase(BaseModel):
+    id: int = Field(description="Unique user identifier")
     username: str = Field(min_length=3, max_length=20, description="Unique username")
     email: EmailStr= Field(description="User email for authorization")
     full_name: Optional[str] = Field(default=None,max_length=50,description='User full name')
@@ -13,7 +14,6 @@ class UserCreate(UserBase):
     password: str = Field(min_length=8,pattern=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$'
 , description='User password for authorization')
 class UserResponse(UserBase):
-    id: int = Field(description="Unique user identifier")
     is_active: bool = Field(description='Shows user activity ')
     created_at: datetime
     model_config = {"from_attributes":True}
