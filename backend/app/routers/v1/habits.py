9cca11fc8b27a95aa9b9fea018af5d317bcc27e5
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-02 18:37:49 +0500

    Added imports. Create habit_id

--- backend/app/routers/v1/habits.py
+++ backend/app/routers/v1/habits.py
@@ -1,7 +1,23 @@
-from fastapi import APIRouter
+from app.core.database import get_db
+from app.core.security import get_current_user
+from app.schemas.habit import HabitUpdate
+from app.schemas.user import UserResponse
+from app.services.habit_service import HabitService
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 @router.get("/test-habits")
-async def test_habits():
-    return {"message": "Habits router работает"}
+async def test_habits(current_user: UserResponse = Depends(get_current_user)):
+    return {"message": "Habits router работает", "current_user_id": current_user.id}
+
+@router.put("/{habit_id}")
+async def update_habit(
+    habit_id: int,
+    habit_update: HabitUpdate,
+    current_user: UserResponse = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    habit_service = HabitService(db)
+    return habit_service.update_habit(habit_id, habit_update, current_user.id)
