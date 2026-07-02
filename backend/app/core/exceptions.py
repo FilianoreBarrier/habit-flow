9f2267aaa7e5aac26e8ff6ee9e7a3697dffb67b9
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-02 18:37:16 +0500

    Add raise_habit_not_found exception

--- backend/app/core/exceptions.py
+++ backend/app/core/exceptions.py
@@ -3,7 +3,13 @@ from fastapi import HTTPException, status
 
 def raise_user_not_found(user_id: int):
     raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"User with id {user_id} not found."
+        status_code = status.HTTP_404_NOT_FOUND,
+        detail = f"User with id {user_id} not found."
     )
 
+def raise_habit_not_found(habit_id: int):
+    raise HTTPException(
+                status_code = status.HTTP_404_NOT_FOUND,
+                detail = f"Habit with id {habit_id} not found."
+
+            )
