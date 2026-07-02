fd147c8bfa7de71fcb81cda07011c213c9a1ab5c
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-02 18:36:49 +0500

    Added create_habit and update_habit methods

--- backend/app/services/habit_service.py
+++ backend/app/services/habit_service.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from app.core.exceptions import raise_user_not_found
+from app.core.exceptions import raise_habit_not_found, raise_user_not_found
 from app.repositories.habit_repository import HabitRepository
 from app.repositories.user_repository import UserRepository
 from app.schemas.habit import HabitCreate, HabitResponse, HabitUpdate
@@ -14,11 +14,11 @@ class HabitService:
         self.user_repository = UserRepository(db)   # может понадобиться для проверок
 
     def create_habit(self, habit_data: HabitCreate, user_id: int) -> HabitResponse:
-        # Здесь должна быть логика:
-        # - проверить, существует ли пользователь
-        # - проверить лимит привычек у пользователя (опционально)
-        # - создать привычку
-        ...
+        if not self.user_repository.get_by_id(user_id):
+            raise_user_not_found(user_id)
+        habit = self.habit_repository.create(habit_data, user_id)
+        return HabitResponse.model_validate(habit)
+
 
     def get_user_habits(self, user_id: int) -> list[HabitResponse]:
         ...
@@ -26,8 +26,17 @@ class HabitService:
     def get_habit_by_id(self, habit_id: int) -> HabitResponse:
         ...
 
-    def update_habit(self, habit_id: int, habit_update: HabitUpdate) -> HabitResponse:
-        ...
+    def update_habit(self, habit_id: int, habit_update: HabitUpdate, user_id: int) -> HabitResponse:
+        habit = self.habit_repository.get_by_id(habit_id)
+        if not habit:
+            raise_habit_not_found(habit_id)    
+        if habit.user_id != user_id: # type: ignore[attr-defined]
+            raise HTTPException(
+                status_code = status.HTTP_403_FORBIDDEN,
+                detail = 'You can only update own habits! '
+            )
+        updated_habit = self.habit_repository.update(habit_id, habit_update)
+        return HabitResponse.model_validate(updated_habit)
 
     def delete_habit(self, habit_id: int) -> bool:
         ...
