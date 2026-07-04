ee25d2163ad5f334e67cde150114cfbaa6a9c501
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-04 18:08:58 +0500

    Added endpoints for authentication

--- backend/app/routers/v1/auth.py
+++ backend/app/routers/v1/auth.py
@@ -1,24 +1,30 @@
 from app.core.database import get_db
-from app.core.security import get_current_user
+from app.core.security import create_access_token, get_current_user
 from app.schemas.user import LoginSchema, UserCreate, UserResponse
 from app.services.user_service import UserService
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
 @router.post("/register", response_model=UserResponse,status_code=status.HTTP_201_CREATED)
-def register_user(user_data: UserCreate, db:Session = Depends(get_db)):
+def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
     user_service = UserService(db)
     return user_service.create_user(user_data)
 
-@router.post('/login',status_code=status.HTTP_200_OK)
+@router.post('/login', status_code=status.HTTP_200_OK)
 def login_user(
-    login_data:LoginSchema,
+    login_data: LoginSchema,
     db: Session = Depends(get_db)
     ):
 
     user_service = UserService(db)
     user = user_service.authenticate_user(login_data.email, login_data.password)
 
-    access_token = create_ac
+    access_token = create_access_token(data={"sub": str(user.id)})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user": UserResponse.model_validate(user)
+    }
