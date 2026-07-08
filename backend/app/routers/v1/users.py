71d70912aa12609ffe1d093b569bb45dfd8b119c
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-08 18:58:10 +0500

    Add endpoints and imports

--- backend/app/routers/v1/users.py
+++ backend/app/routers/v1/users.py
@@ -1,10 +1,11 @@
-from app.core.database import get_db
-from app.schemas.user import UserCreate, UserResponse
-from app.services.user_service import UserService
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from app.core.database import get_db
+from app.core.security import get_current_user, get_password_hash
+from app.schemas.user import UserResponse, UserUpdate, ChangePasswordSchema
+from app.services.user_service import UserService
 
-router = APIRouter()
+router = APIRouter(prefix='/users', tags=['users'])
 
 
 @router.get("/test", tags=["users"])
@@ -12,8 +13,35 @@ async def test_users():
     return {"message": "Users router работает"}
 
 
-# Пример будущего эндпоинта
-@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def register_user(user_data: UserCreate, db: Session = Depends(get_db)):
+@router.get("/me", response_model=UserResponse)
+def get_current_user_info(current_user: UserResponse = Depends(get_current_user)):
+    """Получить информацию о текущем пользователе"""
+    return current_user
+
+@router.patch('/me')
+def update_user(
+    user_update: UserUpdate,
+    current_user: UserResponse = Depends(get_current_user),
+    db: Session = Depends(get_db)
+    ):
     user_service = UserService(db)
-    return user_service.create_user(user_data)
+    return user_service.update_user(current_user.id, user_update)
+
+@router.post('/me/change-password',response_model=UserResponse, status_code=status.HTTP_200_OK)
+def change_user_password(
+    change_data: ChangePasswordSchema,
+    current_user: UserResponse = Depends(get_current_user),
+    db: Session = Depends(get_db)
+    ):
+    user_service = UserService(db)
+    return user_service.change_password(current_user.id, change_data.old_password, change_data.new_password)
+
+@router.post('/me/deactivate', response_model=UserResponse, status_code=status.HTTP_200_OK)
+def deactivate_user(db: Session = Depends(get_db),current_user:UserResponse = Depends(get_current_user)):
+    user_service = UserService(db)
+    return user_service.deactivate_user(current_user.id)
+
+@router.get('/{user_id}',response_model=UserResponse, status_code=status.HTTP_200_OK)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user_service = UserService(db)
+    return user_service.get_by_id(user_id)
