f3aaf14282eb56cfb8709a266d4c38594e20878b
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-08 18:58:31 +0500

    Added ChangePasswordSchema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -5,19 +5,20 @@ from pydantic import BaseModel, EmailStr, Field
 
 
 class UserBase(BaseModel):
-    id: int = Field(description="Unique user identifier")
     username: str = Field(min_length=3, max_length=20, description="Unique username")
     email: EmailStr= Field(description="User email for authorization")
     full_name: Optional[str] = Field(default=None,max_length=50,description='User full name')
-    
+
 class UserCreate(UserBase):
     password: str = Field(min_length=8,pattern=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$'
 , description='User password for authorization')
+
 class UserResponse(UserBase):
+    id: int = Field(description="Unique user identifier")
     is_active: bool = Field(description='Shows user activity ')
     created_at: datetime
     model_config = {"from_attributes":True}
-    
+
 
 class UserUpdate(BaseModel):
     full_name: Optional[str] = None
@@ -28,3 +29,7 @@ class UserUpdate(BaseModel):
 class LoginSchema(BaseModel):
     email: EmailStr = Field(description="Email for login")
     password: str = Field(description="Password for login")
+
+class ChangePasswordSchema(BaseModel):
+    old_password: str = Field(min_length=8,pattern=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$')
+    new_password: str = Field(min_length=8,pattern=r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$')
