12fa6f631c73b811376509572f142f7479498e27
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-19 16:53:25 +0500

    Added model for blacklist_token

---
+++ backend/app/models/token_blacklist.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+
+class TokenBlackList(Base):
+    __tablename__ = 'token_blacklist'\
+
+    id:Mapped[int] = mapped_column(primary_key=True,index=True)
+    token: Mapped[str] = mapped_column(String(500),index=True, unique=True)
+    expires_at: Mapped[datetime] = mapped_column(DateTime)
