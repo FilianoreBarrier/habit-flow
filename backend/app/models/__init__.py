f9bd3acb25a73440daecf74df3d255b17dd7ee0a
FilianoreBarrier <maric.zubenko@gmail.com>
2026-07-21 02:32:06 +0500

    added TokenBlackList

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,6 @@
 from app.models.habit import Habit
 from app.models.habit_log import HabitLog
 from app.models.user import User
+from app.models.token_blacklist import TokenBlackList
 
-
-__all__ = ["User", "Habit", "HabitLog"]
+__all__ = ["User", "Habit", "HabitLog", "TokenBlackList"]
